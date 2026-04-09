44c02dec8a09429b119e55e1765483317a26153c
ktomek <tiacocstoe@gmail.com>
2026-04-09 19:29:55 +0200

    shoper lib patch for heroku

--- config/env.php
+++ config/env.php
@@ -2,3 +2,7 @@
 
 defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG') !== false ? (bool)getenv('YII_DEBUG') : true);
 defined('YII_ENV')   or define('YII_ENV',   getenv('YII_ENV') ?: 'dev');
+
+// Suppress E_DEPRECATED so abandoned vendor libs (dreamcommerce/shop-appstore-lib)
+// don't crash via Yii2's error handler converting notices to exceptions.
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
